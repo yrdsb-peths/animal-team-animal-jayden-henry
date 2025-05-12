5f9aa402b732bf38bfa79470e200b6641a2c2805
henw24 <349581223@gapps.yrdsb.ca>
2025-05-12 09:04:26 -0400

    elephant

---
+++ Elephant.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class elephant here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("a") ){
+            move(-1);
+        }
+        if(Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("d") ){
+            move(1);
+        }
+        
+    }
+}
